ca61e57e1aa03d0b89a3b1f84939d95325b16cc2
MichaelH9De <michaelr@stevenhunt.com>
2026-03-26 16:14:53 +0000

    Update localStorage.ts

--- lib/storage/localStorage.ts
+++ lib/storage/localStorage.ts
@@ -1,1 +1,12 @@
-export const localStorage = {}
+export const localStorageApi = {
+  get: (key: string) => {
+    if (typeof window === 'undefined') return null
+    return window.localStorage.getItem(key)
+  },
+  set: (key: string, value: string) => {
+    if (typeof window === 'undefined') return
+    window.localStorage.setItem(key, value)
+  },
+}
+
+export const localStorage = localStorageApi
